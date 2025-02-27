ef8f61e06418b0ed2465f29d759c599962761929
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-02-28 02:22:25 +0700

    build: create routes

---
+++ api/v1/routes/task.route.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+const Task = require("../../../models/taks.model");
+
+
+// const controller = require("../../controllers/client/task.controller");
+
+router.get("/", async (req, res) => {
+  const tasks = await Task.find({
+    deleted: false,
+  });
+  console.log(tasks);
+  res.json(tasks);
+});
+
+router.get("/detail/:id", async (req, res) => {
+  const id = req.params.id;
+  const tasks = await Task.findOne({
+    _id: id,
+    deleted: false,
+  });
+  console.log(tasks);
+  res.json(tasks);
+});
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 const express = require("express");
-require("dotenv").config();
 const database = require("./config/database");
+require("dotenv").config();
+const routesApiVer1 = require("./api/v1/routes/index.route");
 const app = express();
 const port = process.env.PORT;
 
-const Task = require("./models/taks.model");
-
 database.connect();
 
-app.get("/tasks", async (req, res) => {
-  const tasks = await Task.find({
-    deleted: false,
-  });
-  console.log(tasks);
-  res.json(tasks);
-});
-
-app.get("/tasks/detail/:id", async (req, res) => {
-  const id = req.params.id;
-  const tasks = await Task.findOne({
-    _id: id,
-    deleted: false,
-  });
-  console.log(tasks);
-  res.json(tasks);
-});
+// Routes Version 1
+routesApiVer1(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
