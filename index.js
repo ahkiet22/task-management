b6d41f547d1621dd2153408297754d3979c0acc1
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-03-01 16:57:02 +0700

    vendor: install and setup cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const database = require("./config/database");
 require("dotenv").config();
 const routesApiVer1 = require("./api/v1/routes/index.route");
@@ -8,12 +9,13 @@ const bodyParser = require("body-parser");
 
 database.connect();
 
+app.use(cors());
+
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Routes Version 1
 routesApiVer1(app);
-
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
